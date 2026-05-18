2ddbbb596324e9b290b4b45fc7141ef434f79430
juanbetancourt1049 <juanbetancourt315@gmail.com>
2026-05-18 13:46:03 -0500

    configuracion variables de entorno apra el back

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://opm-env.eba-ywfhqwtf.us-east-1.elasticbeanstalk.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://opm-env.eba-ywfhqwtf.us-east-1.elasticbeanstalk.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
